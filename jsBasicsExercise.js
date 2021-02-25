31959452a5fe315895c48fb78dc7db2b408b20db
Isaiah Robinson <isaiahrobinson@KCCCs-MacBook-Pro-8.local>
2021-02-25 14:18:11 -0500

    Completed JS Basics exercise

---
+++ jsBasicsExercise.js
@@ -0,0 +1,16 @@
+console.log(4);
+
+// Strings, Null, Booleans, Numbers, Undefined
+
+// adding(+), subtracting(-), multiplying(*), dividing(/), exponents(**), and modulus(% remainder)
+
+console.log(5%2);
+console.log(5%4);
+
+console.log('I\'m Mary Poppins, Y\'all');
+
+console.log("Wanda" + "Vision");
+
+console.log("Isaiah".length + "Robinson".length);
+
+console.log("Avengers: Endgame"[16]);
